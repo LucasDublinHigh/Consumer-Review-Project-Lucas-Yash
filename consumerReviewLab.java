5c233fc5ebebb9c9d10122503712439b5de4aa3a
LucasDublinHigh <148259944+LucasDublinHigh@users.noreply.github.com>
2025-10-22 13:16:50 -0700

    Add reviews for iPhone and Android in consumerReviewLab

---
+++ consumerReviewLab.java
@@ -0,0 +1,16 @@
+public static void main(String[] args) {
+    String[] iphoneReviews = {
+        "while on IOS you just need to setup sleep mode in 15m minutes, otherwise sending will be aborted when display goes black, or you open another app. When i first tried IOS i was excited but in few days i realised that this OS so limited, for sake of battery longevity, or i dont know why.",
+        "I currently have the iphone 16 pro. Siri is terrible and I hate the stock keyboard. I use Swiftkey. I also like ios notes app.",
+        "I was liking iOS more it was easier but i miss universal back gesture,installing apps and so on… when iOS 26 beta arrived i install it and omg i hate it everything needs more clicks.",
+
+    };
+    String[] androidReviews = {
+       "Personally, i like Android more. Consistent gestures, flexibility, you can send 1GB video and use some other apps at the same time",
+        " I'm considering going back to the Samsung line - maybe after the galaxy 26 line is introduced. Android does have a lot of bloatware, but I don't use my phone for business or work.",
+        "I've always been on Android, though did strongly consider a Blackberry when I got my first smartphone. As far as cameras go though this is really gonna be down to the specific phone. They vary so wildly. I know there's plenty of vids on YouTube comparing phone cameras though so those could be helpful.",
+        "I have tried pretty much all flagship phones this year, s25 ultra, fold 7, pixel 10 pro XL, one plus 13. Each time I think this is the one I realize just how buggy and unstable the experience is, both due to third party apps just lacking polish and the OS experience not being fluid. Android has way more features and flexibility but boy it is a buggy, stuttery and poorly supported mess. Third party apps feel like an after thought, with poorly designed UX, less features and tons of bugs.",
+        
+
+    };
+}
